5290c2a785510ba6b4442f791dae736e132c8b49
SolaireRocks <109108467+SolaireRocks@users.noreply.github.com>
2025-05-29 13:17:40 -0400

    33

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ import { getFunctions, httpsCallable } from "firebase/functions";
 
 // Your web app's Firebase configuration (remains the same)
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
